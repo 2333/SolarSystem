7c68f3eeda62cb9f62f9f66448ec079a294c6142
2333 <zk.amaranth@gmail.com>
2014-11-01 21:23:17 +0800

    earth view

---
+++ TextBoard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HelixToolkit.Wpf;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace SolarsystemDemo
+{
+    class TextBoard
+    {
+        public  TextBoard()
+        {
+            ;
+        }
+    }
+}
